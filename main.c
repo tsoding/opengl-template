6a03328abfc207aa5c4b0b100e524baba48c8e52
rexim <reximkut@gmail.com>
2021-03-06 01:55:56 +0700

    Add debug logging

--- main.c
+++ main.c
@@ -163,6 +163,23 @@ void window_size_callback(GLFWwindow* window, int width, int height)
         SCREEN_HEIGHT);
 }
 
+void MessageCallback(GLenum source,
+                     GLenum type,
+                     GLuint id,
+                     GLenum severity,
+                     GLsizei length,
+                     const GLchar* message,
+                     const void* userParam)
+{
+    (void) source;
+    (void) id;
+    (void) length;
+    (void) userParam;
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+            (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+            type, severity, message);
+}
+
 int main()
 {
     if (!glfwInit()) {
@@ -188,6 +205,9 @@ int main()
         fprintf(stderr, "Could not initialize GLEW!\n");
         exit(1);
     }
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
 
     reload_shaders();
 
