af1f340d4c7a41b1ddda2526ebdc16d44055ced7
rexim <reximkut@gmail.com>
2021-04-21 02:16:28 +0700

    Implement constructors for V2 and RGBA

--- oglt_math.h
+++ oglt_math.h
@@ -7,11 +7,21 @@ typedef struct {
     float x, y;
 } V2;
 
+static inline V2 v2(float x, float y)
+{
+    return (V2) {.x = x, .y = y};
+}
+
 #define RGBA_COUNT 4
 
 typedef struct {
     float r, g, b, a;
 } RGBA;
+
+static inline RGBA rgba(float r, float g, float b, float a)
+{
+    return (RGBA) {.r = r, .g = g, .b = b, .a = a};
+}
 
 static inline float lerp(float a, float b, float t)
 {
