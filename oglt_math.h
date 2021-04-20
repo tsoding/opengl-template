f677b694fcf275a625e832a7dd031fd4f0e5687c
rexim <reximkut@gmail.com>
2021-04-21 01:49:27 +0700

    Introduce V2 type

--- oglt_math.h
+++ oglt_math.h
@@ -1,6 +1,12 @@
 #ifndef OGLT_MATH_H_
 #define OGLT_MATH_H_
 
+#define V2_COUNT 2
+
+typedef struct {
+    float x, y;
+} V2;
+
 static inline float lerp(float a, float b, float t)
 {
     return a + (b - a) * t;
