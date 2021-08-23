f869941ae972cae329b722d512b2dba4a0e7573f
rexim <reximkut@gmail.com>
2021-08-24 05:25:00 +0700

    Remove GLEW dependency on Windows

--- setup_glfw.bat
+++ setup_glfw.bat
@@ -8,12 +8,3 @@ mkdir Dependencies\GLFW\include\GLFW
 move glfw-3.3.2.bin.WIN64\include\GLFW\glfw3.h Dependencies\GLFW\include\GLFW\glfw3.h
 del glfw-3.3.2.bin.WIN64.zip
 rmdir /s /q glfw-3.3.2.bin.WIN64
-
-curl -fsSL -o glew-2.1.0-win32.zip https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-win32.zip/download
-tar -xf glew-2.1.0-win32.zip
-mkdir Dependencies\GLEW\lib\
-move glew-2.1.0\lib\Release\x64\glew32s.lib Dependencies\GLEW\lib\glew32s.lib
-mkdir Dependencies\GLEW\include\GL\
-move glew-2.1.0\include\GL\glew.h Dependencies\GLEW\include\GL\glew.h
-del glew-2.1.0-win32.zip
-rmdir /s /q glew-2.1.0
